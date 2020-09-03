51b62b4b8d76a58530660dd7f954b597777ce00f
Tal Har Hol <talharhol@gmail.com>
2020-09-03 10:10:25 +0300

    change config

--- PDFsender/Consts.cs
+++ PDFsender/Consts.cs
@@ -15,9 +15,9 @@ namespace ChooseName
         public const string PrintValue = "#";
         public const string ExcelPassword = "1234";
         public static string DesktopLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        public const string PrintName = @"\הודעת_חיוב_להדפסה.pdf";
-        public const string Subject = "הודעת חיוב ";
-        public const string Title = "הודעת חיוב";
+        public const string PrintName = @"\חיוב_חודשי_להדפסה.pdf";
+        public const string Subject = "חיוב חודשי מעיין ברוך ";
+        public const string Title = "חיוב חודשי";
         public const string EndOfPageSeperator = "כ\"הס";
         public static System.util.RectangleJ AccountArea = new System.util.RectangleJ(310, 680, 230, 22);
         public static Regex AccountRegex = new Regex(@"(?<Account>[0-9]{9,})", RegexOptions.Compiled);
